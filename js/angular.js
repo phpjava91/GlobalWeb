ef8e2a2dc5643a3e89cf9d31195ba6b94a25aec1
phpjava91 <phpjava55@hotmail.com>
2016-01-29 12:51:35 -0500

    GlobalWeb

---
+++ js/angular.js
@@ -0,0 +1,22 @@
+(function(){
+
+
+  var app = angular.module('services',[]);
+
+  app.controller('servicesController',function(){
+     this.tab = 1;	
+    
+    this.selectedTab = function(tab){
+       this.tab = tab;
+    };
+  });
+
+
+
+})();
+
+
+
+
+
+
